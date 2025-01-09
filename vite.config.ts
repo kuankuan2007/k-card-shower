5205c43652ba271d1e19676e6e87642dfdacf5d5
kuankuan2007 <2163826131@qq.com>
2025-01-10 04:25:34 +0800

    add rollup-plugin-visualizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import AutoImport from 'unplugin-auto-import/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,13 @@ export default defineConfig({
     AutoImport({
       imports: ['vue', 'vue-router'],
       dts: './src/types/auto-imports.d.ts',
+    }),
+    visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: false,
+      filename: './temp/test.html',
+      open: true,
     }),
   ],
   resolve: {
